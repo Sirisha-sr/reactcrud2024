4292fe0248ccb79dd79fc1438c953c4e2d0381d7
Sirisha Thatiparthi <sirithatiparthi14@gmail.com>
2024-11-09 16:33:03 +0530

    Create SirishaT.jsx

---
+++ src/assets/SirishaT.jsx
@@ -0,0 +1,27 @@
+const SirishaT = () => {
+    return(
+        <div>
+            <h1>SirishaT component</h1>
+        </div>
+    )
+}
+export default SirishaT;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
